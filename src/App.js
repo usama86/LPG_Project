d3b5a4ea1faedb43000624b29bdae59bacdcc2c1
Muhammad Usama <usama.amjad32@gmail.com>
2019-10-08 13:52:02 +0500

    f
    f

--- src/App.js
+++ src/App.js
@@ -13,24 +13,31 @@ const Login = React.lazy(() => import('./views/Pages/Login'));
 const Register = React.lazy(() => import('./views/Pages/Register'));
 const Page404 = React.lazy(() => import('./views/Pages/Page404'));
 const Page500 = React.lazy(() => import('./views/Pages/Page500'));
+const CustomerSide = React.lazy(() => import('./views2/Dashboard/DashboardCust'));
 
 class App extends Component {
-
-  render() {
-    return (
-      <HashRouter>
-          <React.Suspense fallback={loading()}>
-            <Switch>
-              <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
-              <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
-              <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
-              <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
-              <Route path="/" name="Home" render={props => <DefaultLayout {...props}/>} />
-            </Switch>
-          </React.Suspense>
-      </HashRouter>
-    );
-  }
+	//making customer panel
+	render() {
+		return (
+			<HashRouter>
+				<React.Suspense fallback={loading()}>
+					<Switch>
+						<Route exact path="/login" name="Login Page" render={(props) => <Login {...props} />} />
+						<Route
+							exact
+							path="/register"
+							name="Register Page"
+							render={(props) => <Register {...props} />}
+						/>
+						<Route exact path="/404" name="Page 404" render={(props) => <Page404 {...props} />} />
+						<Route exact path="/500" name="Page 500" render={(props) => <Page500 {...props} />} />
+						<Route path="/" name="Home" render={(props) => <CustomerSide {...props} />} />
+						<Route path="/abc" name="Home" render={(props) => <DefaultLayout {...props} />} />
+					</Switch>
+				</React.Suspense>
+			</HashRouter>
+		);
+	}
 }
 
 export default App;
---
+++ src/views2/Dashboard/DashboardCust.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			dropdownOpen: false,
+			radioSelected: 2
+		};
+	}
+
+	render() {
+		return (
+			<div className="animated fadeIn">
+				<span>HI</span>
+			</div>
+		);
+	}
+}
+
+export default Dashboard;
